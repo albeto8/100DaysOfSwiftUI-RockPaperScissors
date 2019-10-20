6dfc2d13c44cb6bb6408cc0c6cf5cd226f405352
Mario Alberto Barragán Espinosa <>
2019-10-20 18:59:43 -0500

    First commit

---
+++ RockPaperScissors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RockPaperScissors
+//
+//  Created by Mario Alberto Barragán Espinosa on 20/10/19.
+//  Copyright © 2019 Mario Alberto Barragán Espinosa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
